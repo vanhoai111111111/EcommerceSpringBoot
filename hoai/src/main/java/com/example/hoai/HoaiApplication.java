e6875efb419de26563eecc16d259aead956ff310
Webfullstack <hoai8eqt@gmail.com>
2025-05-12 11:56:49 +0700

    a

---
+++ hoai/src/main/java/com/example/hoai/HoaiApplication.java
@@ -0,0 +1,13 @@
+package com.example.hoai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoaiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HoaiApplication.class, args);
+    }
+
+}
---
+++ hoai/src/test/java/com/example/hoai/HoaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hoai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoaiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
